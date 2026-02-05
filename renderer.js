3a23e4be83506d237921e2e81fad8227d6f4236a
Annie Rączka <x.aniutka@gmail.com>
2026-02-05 21:00:45 +0100

    add: function that renders the main content based on given var

--- renderer.js
+++ renderer.js
@@ -1,3 +1,35 @@
 document.getElementById('btn-close').addEventListener('click', () => {
   window.electronAPI.closeWindow();
 });
+
+
+const appMain = document.getElementById('main');
+
+function renderMainContnt(view) {
+
+  appMain.innerHTML = '';
+
+  if(view=='drinkChoice'){
+        appMain.innerHTML = `<h1>⊱· CAFEE ORDER ·⊰</h1>
+        <p>select your order:P</p>
+        <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>
+            <button class="btn-drink">
+                <span class="icon">☕</span> Espresso <span class="duration">10 min focus</span>
+            </button>
+            <button class="btn-drink">
+                <span class="icon">🥛</span> Latte <span class="duration">15 min focus</span>
+            </button>
+            <button class="btn-drink">
+                <span class="icon">🧋</span> Tea <span class="duration">25 min focus</span>
+            </button>
+            <button class="btn-drink">
+                <span class="icon">🥤</span> Soda <span class="duration">50 min focus</span>
+            </button>
+        <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>`;
+  }else{
+    appMain.innerHTML = `<h1>App error</h1>`;
+  }
+}
+
+renderMainContnt('drinkChoice');
+
