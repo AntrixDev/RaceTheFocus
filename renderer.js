1b1e4ec52f5764df086eaaff6a4fa2e8f5199f48
Annie Rączka <x.aniutka@gmail.com>
2026-02-05 21:57:31 +0100

    mod: drink name passed to details view

--- renderer.js
+++ renderer.js
@@ -5,7 +5,7 @@ btnClose.addEventListener('click', ()=>{
   window.electronAPI.closeWindow();
 });
 
-function renderMainContnt(view){
+function renderMainContnt(view, drinkData){
   appMain.innerHTML = '';
 
   switch(view){
@@ -13,7 +13,7 @@ function renderMainContnt(view){
       appMain.innerHTML = ViewMenu();
       break;
     case 'details':
-      appMain.innerHTML = ViewDetails();
+      appMain.innerHTML = ViewDetails(drinkData);
       break;
     default:
       appMain.innerHTML = '<p>App error</p>';
@@ -22,25 +22,27 @@ function renderMainContnt(view){
 
 function ViewMenu(){
   return `<h1>⊱· CAFEE ORDER ·⊰</h1>
-      <p>select your order:P</p>
-      <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>
-          <button class="btn-drink" onclick="renderMainContnt('details')">
-              <span class="icon">☕</span> Espresso <span class="duration">10 min focus</span>
-          </button>
-          <button class="btn-drink" onclick="renderMainContnt('details')">
-              <span class="icon">🥛</span> Latte <span class="duration">15 min focus</span>
-          </button>
-          <button class="btn-drink" onclick="renderMainContnt('details')">
-              <span class="icon">🧋</span> Tea <span class="duration">25 min focus</span>
-          </button>
-          <button class="btn-drink" onclick="renderMainContnt('details')">
-              <span class="icon">🥤</span> Soda <span class="duration">50 min focus</span>
-          </button>
-      <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>`;
+    <p>select your order:P</p>
+    <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>
+      <button class="btn-drink" onclick="renderMainContnt('details', 'Espresso')">
+          <span class="icon">☕</span> Espresso <span class="duration">10 min focus</span>
+      </button>
+      <button class="btn-drink" onclick="renderMainContnt('details', 'Latte')">
+          <span class="icon">🥛</span> Latte <span class="duration">15 min focus</span>
+      </button>
+      <button class="btn-drink" onclick="renderMainContnt('details', 'Tea')">
+          <span class="icon">🧋</span> Tea <span class="duration">25 min focus</span>
+      </button>
+      <button class="btn-drink" onclick="renderMainContnt('details', 'Soda')">
+          <span class="icon">🥤</span> Soda <span class="duration">50 min focus</span>
+      </button>
+    <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>`;
 }
 
-function ViewDetails(){
-  return `<p>details vieww</p> <button class="btn-drink" onclick="renderMainContnt('menu')">←Back</button>`
+function ViewDetails(drinkName){
+  return `<p>details view</p> 
+    <p>Selectd: ${drinkName}</p>
+    <button class="btn-drink" onclick="renderMainContnt('menu')">←Back</button>`
 }
 
 renderMainContnt('menu');
