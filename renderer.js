3fd461b7895473406d968afb3e686b7db064a39b
Annie Rączka <x.aniutka@gmail.com>
2026-02-05 21:37:34 +0100

    add: new view function for details

--- renderer.js
+++ renderer.js
@@ -5,22 +5,26 @@ document.getElementById('btn-close').addEventListener('click', () => {
 const appMain = document.getElementById('main');
 
 function ViewMenu(){
-    return `<h1>⊱· CAFEE ORDER ·⊰</h1>
-        <p>select your order:P</p>
-        <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>
-            <button class="btn-drink" onclick="renderMainContnt('test')">
-                <span class="icon">☕</span> Espresso <span class="duration">10 min focus</span>
-            </button>
-            <button class="btn-drink">
-                <span class="icon">🥛</span> Latte <span class="duration">15 min focus</span>
-            </button>
-            <button class="btn-drink">
-                <span class="icon">🧋</span> Tea <span class="duration">25 min focus</span>
-            </button>
-            <button class="btn-drink">
-                <span class="icon">🥤</span> Soda <span class="duration">50 min focus</span>
-            </button>
-        <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>`;
+  return `<h1>⊱· CAFEE ORDER ·⊰</h1>
+      <p>select your order:P</p>
+      <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>
+          <button class="btn-drink" onclick="renderMainContnt('details')">
+              <span class="icon">☕</span> Espresso <span class="duration">10 min focus</span>
+          </button>
+          <button class="btn-drink" onclick="renderMainContnt('details')">
+              <span class="icon">🥛</span> Latte <span class="duration">15 min focus</span>
+          </button>
+          <button class="btn-drink" onclick="renderMainContnt('details')">
+              <span class="icon">🧋</span> Tea <span class="duration">25 min focus</span>
+          </button>
+          <button class="btn-drink" onclick="renderMainContnt('details')">
+              <span class="icon">🥤</span> Soda <span class="duration">50 min focus</span>
+          </button>
+      <p class="dots">⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯</p>`;
+}
+
+function ViewDetails(){
+  return `<p>details view</p>`;
 }
 
 function renderMainContnt(view){
@@ -30,8 +34,8 @@ function renderMainContnt(view){
     case 'menu':
       appMain.innerHTML = ViewMenu();
       break;
-    case 'test':
-      appMain.innerHTML = 'passed';
+    case 'details':
+      appMain.innerHTML = ViewDetails();
       break;
     default:
       appMain.innerHTML = '<p>App error</p>';
